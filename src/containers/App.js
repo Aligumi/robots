70fc51b278d1b5a43c5ecde7fccbe51711d01862
Aligumi <aaligumi@gmail.com>
2022-09-27 14:19:42 +0100

    Updated the code with React Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 // import { robot } from './robot.js';
 import CardList from '../components/CardList.js';
 import "./App.css";
@@ -6,30 +6,37 @@ import SearchBox from '../components/SearchBox.js';
 import Scroll from "../components/Scroll.js";
 import ErrorBoundary from '../components/ErrorBoundary.js';
 
-class App extends Component  {
-  constructor() {
-    super()
-    this.state = {
-      robot: [],
-      searchField: ""
-    }
-  }
+function App() {
+  
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value})
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     robot: [],
+  //     searchField: ""
+  //   }
+  // }
+  const [robot, setRobot] = useState([])
+  const [searchField, setSearchField] = useState("")
+
+ const onSearchChange = (event) => {
+    setSearchField(event.target.value)
 }
 
-componentDidMount() {
-  fetch('https://jsonplaceholder.typicode.com/users').then(response => {
-    return response.json();
-  })
-  .then(users => {
-    this.setState({ robot: users})
-  })
-}
+// componentDidMount() {
+//   fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+//     return response.json();
+//   })
+//   .then(users => {
+//     this.setState({ robot: users})
+//   })
+// }
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {return response.json();})
+   .then(users => {setRobot(users)});
+  }, [robot, searchField])
 
-  render() {
-    const { robot, searchField } = this.state;
     const filteredRobots = robot.filter(robot => {
       return robot.name.toLowerCase()
       .includes(searchField.toLowerCase());
@@ -39,7 +46,7 @@ componentDidMount() {
       (
         <div className='tc' >
             <h1 className='f1'>RoboHomies</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
+            <SearchBox searchChange={onSearchChange}/>
             <Scroll>
               <ErrorBoundary>
                 <CardList robot={filteredRobots}/>
@@ -49,6 +56,6 @@ componentDidMount() {
       );
 
     }
-}
+
  
 export default App
